cdf66beb2603c940e96d7adbc968757f939836b4
Daljit-k <49802608+Daljit-k@users.noreply.github.com>
2019-05-08 17:53:00 +0530

    Update settings.py

--- Pears/settings.py
+++ Pears/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't=52e6!4i=b#hlhrm)bksvlt1%^jvs!dyz(nyv(*n7!4o(oqcw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
